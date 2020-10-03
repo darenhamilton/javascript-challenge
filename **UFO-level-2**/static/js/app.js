60ddbea97221f688e1d472b6fc311abb882fd114
Hamilton <hamihomeme@gmail.com>
2020-10-03 15:03:28 -0700

    Finished level one

--- **UFO-level-2**/static/js/app.js
+++ **UFO-level-2**/static/js/app.js
@@ -1,9 +1,6 @@
-// from data.js
+/// from data.js
 let tableData = data;
-    
-
-
-
+ 
 // Use D3 to select the table body
 let tbody = d3.select("tbody");
 
@@ -16,39 +13,29 @@ data.forEach((ufoSighting) => {
   });
 });
   
-/// Select the button
+/// Select the button and form entry
 let button = d3.select("#filter-btn"),
     form = d3.select("#form");
 const runEnter = () => {
     
    // Prevent the page from refreshing
-  d3.event.preventDefault(),  
+  d3.event.preventDefault();  
   //remove items from table
   tbody.html("");
 
-  
-
     // Select the input date get the
-    let inputElement = d3.selectAll("#datetime");
-    let inputCity = d3.selectAll("#city");
-    
-
-    // Get the value property of the input date, state, shape
+    let inputElement = d3.select("#datetime");
+    // Get the value property of the input date
     let inputValue = inputElement.property("value");
-    let outputCity = inputCity.property("value");
     
     // Filter Data with datetime equal to input value
-    let filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue || 
-                                                       ufoSighting.city === inputValue || 
-                                                       ufoSighting.state === inputValue || 
-                                                       ufoSighting.country === inputValue || 
-                                                       ufoSighting.shape === inputValue);
-    console.log(filteredData);
+    let filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === inputValue);
+    
     filteredData.forEach(function(selections) {
 
     // Append one table row for each object
     let row = tbody.append("tr");
-
+    // console log  values
     Object.entries(selections).forEach(function([key, value]) {
         // Append a cell to the row for each value
         let cell = row.append("td");
